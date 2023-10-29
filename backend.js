a4fea0be52e093dbad2103a54b9d3b88ec357220
Lucas <67701018+lucasyoboy@users.noreply.github.com>
2023-10-29 13:30:30 -0300

    Update backend.js

--- backend.js
+++ backend.js
@@ -2,35 +2,38 @@
 const PocketBase = require('pocketbase/cjs')
 var express = require('express');
 var moment = require('moment');
+moment.locale('de')
 const bodyParser = require("body-parser");
 var app = express();
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const pb = new PocketBase('http://192.168.41.250:8090');
 
+// ------------------
 
 // Realtime Data
-app.get('/realtime', async function(req, res) {
-    try{
+app.get('/realtime', async function (req, res) {
+    try {
         // fetch a paginated records list
         const resultList = await pb.collection('wetterdaten_realtime').getList(1, 1, {
             sort: '-created',
         });
-        res.json({ 
+        res.json({
             temperature: resultList.items[0].temperature,
             humidity: resultList.items[0].humidity,
             pressure: resultList.items[0].pressure,
             altitude: resultList.items[0].altitude,
-            time: resultList.items[0].created,
+            date: moment(resultList.items[0].created).format('MMMM Do YYYY'),
+            time: moment(resultList.items[0].created).format('h:mm:ss a'),
         })
         console.log(resultList.items);
-    }catch(err){
+    } catch (err) {
         console.log(err);
     }
-  });
+});
 
-  app.get('/history', function(req, res) {
+// Histoty Data
+app.get('/history', function (req, res) {
     (async () => {
         temp = [];
         pressure = [];
@@ -50,22 +53,23 @@ app.get('/realtime', async function(req, res) {
             altitude.push([moment(value.created).valueOf(), value.altitude]);
         });
 
-        res.json({ 
-                temperature: temp,
-                windspeed: windspeed,
-                pressure: pressure,
-                humidity: humidity,
-                altitude: altitude
+        res.json({
+            temperature: temp,
+            windspeed: windspeed,
+            pressure: pressure,
+            humidity: humidity,
+            altitude: altitude
         })
     })().catch(err => {
         console.log(err);
     });
-  });
+});
 
-    app.post('/post', (req, res) => {
-        res.send(req.body)
-        pb.collection('wetterdaten_realtime').create({"temperature": Math.round(req.body.temperature),"pressure": Math.round(req.body.pressure),"humidity": Math.round(req.body.humidity),"altitude": Math.round(req.body.altitude), "windspeed": 0, "status": 1});
-    })
+// Post Data from Station
+app.post('/post', (req, res) => {
+    res.send(req.body)
+    pb.collection('wetterdaten_realtime').create({ "temperature": Math.round(req.body.temperature), "pressure": Math.round(req.body.pressure), "humidity": Math.round(req.body.humidity), "altitude": Math.round(req.body.altitude), "windspeed": 0, "status": 1 });
+})
 
 // ------------------------------------------------------------------------------------ //
 
